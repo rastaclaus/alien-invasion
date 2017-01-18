ae780fac1510b5afe8331abafa550515fcf779bd
ArtemS <tema.rastaclaus@gmail.com>
2017-01-18 22:06:01 +0300

    add settings module

---
+++ settings.py
@@ -0,0 +1,10 @@
+"""settings module"""
+
+class Settings:
+    """Класс для хранения настроек игры Alien Invasion. """
+
+    def __init__(self):
+        """Инициализация настроек игры"""
+        self.screen_width = 1000
+        self.screen_hight = 720
+        self.bg_color = (230, 230, 230)
