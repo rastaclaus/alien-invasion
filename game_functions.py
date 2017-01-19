1eae2b604379ae79bdda291730233afc70a76484
Artem <tema.rastaclaus@gmail.com>
2017-01-19 15:21:55 +0300

    Add primitive ship control

--- game_functions.py
+++ game_functions.py
@@ -4,11 +4,16 @@
 import sys
 import pygame
 
-def check_events():
+def check_events(ship):
     """Key press and mouse events processing"""
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             sys.exit()
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_RIGHT:
+                ship.rect.centerx += 1
+            elif event.key == pygame.K_LEFT:
+                ship.rect.centerx -= 1
 
 def update_screen(ai_settings, screen, ship):
     """Refresh and draw screen"""
