c59bfffdf676f7f73812566f80220afc03c4d44d
Artem <tema.rastaclaus@gmail.com>
2017-01-23 17:51:40 +0300

    Add bullet parametres in settings.py

--- settings.py
+++ settings.py
@@ -10,3 +10,8 @@ class Settings:
         self.screen_hight = 720
         self.bg_color = (230, 230, 230)
         self.ship_speed_factor = 1.5
+        # Settings for bullet
+        self.bullet_speed_factor = 1
+        self.bullet_width = 3
+        self.bullet_height = 15
+        self.bullet_color = 60, 60, 60
